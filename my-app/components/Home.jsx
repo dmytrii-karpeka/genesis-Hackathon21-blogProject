ed22781f5fb5262bcce4a64aabee25077191684e
Sergey Skvortsov <skvortsov.serge@gmail.com>
2021-10-23 14:55:17 +0300

    add react-bootstrap

--- my-app/components/Home.jsx
+++ my-app/components/Home.jsx
@@ -1,7 +1,14 @@
+import { Badge } from 'react-bootstrap'
+
 import css from './Home.module.scss';
 
 const Home = () => {
-  return <div className={css.container}><p>Home page</p></div>;
+  return (
+    <div className={css.container}>
+      <p>Home page</p>
+      <Badge bg="secondary">New</Badge>
+    </div>
+  );
 }
  
 export default Home;
