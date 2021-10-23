4cd102ba31aca48157f1abaf296f379272161884
Sergey Skvortsov <skvortsov.serge@gmail.com>
2021-10-23 14:47:47 +0300

    initial next js setup, add scss support

---
+++ my-app/components/Home.jsx
@@ -0,0 +1,7 @@
+import css from './Home.module.scss';
+
+const Home = () => {
+  return <div className={css.container}><p>Home page</p></div>;
+}
+ 
+export default Home;
---
+++ my-app/pages/register/index.js
@@ -0,0 +1,5 @@
+const Registration = () => {
+  return <div>Registration page</div>;
+}
+ 
+export default Registration;
