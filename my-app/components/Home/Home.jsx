f2e8c4fe0c4d16153c7ba756500d910d8dc87472
Sergey Skvortsov <skvortsov.serge@gmail.com>
2021-10-23 20:24:57 +0300

    add infinite scroll

--- my-app/components/Home/Home.jsx
+++ my-app/components/Home/Home.jsx
@@ -1,21 +1,58 @@
-import React, { useEffect } from 'react';
+import * as axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Badge } from 'react-bootstrap'
+import InfiniteScroll from 'react-infinite-scroller';
 
 import css from './Home.module.scss';
 
 import Header from '../Header/Header';
 import { getGamesList } from '../../services/Games.service';
 
+const TestCard = ({ title }) => {
+  return <p>{title}</p>;
+};
+
 const Home = () => {
+  const step = 60;
+  const [allItems, setAllItems] = useState([]);
+  const [visibleItems, setVisibleItems] = useState([]);
+
   useEffect(() => {
-    getGamesList();
+    const fetchItems = async () => {
+      const [err, data] = await getGamesList();
+
+      if (err) return;
+
+      setAllItems(data);
+      setVisibleItems(data.slice(0, step));
+    };
+
+    fetchItems();
   }, []);
+
+  const getMoreItems = () => {
+    const visibleItemsCount = visibleItems.length;
+    const items = [...visibleItems, ...allItems.slice(visibleItemsCount, visibleItemsCount + step)];
+
+    setVisibleItems(items)
+  };
+
+  const renderItems = () => {
+    return visibleItems.map((item) => <TestCard key={item.id} title={item.title} />);
+  };
 
   return (
     <div className={css.container}>
       <Header />
-      <p>Home page</p>
-      <Badge bg="secondary">New</Badge>
+
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={getMoreItems}
+        hasMore={visibleItems.length < allItems.length}
+        loader={<p>Loading...</p>}
+      >
+        {renderItems()}
+      </InfiniteScroll>
     </div>
   );
 }
--- my-app/services/Games.service.js
+++ my-app/services/Games.service.js
@@ -2,10 +2,17 @@ import AxiosAdapter from '../adapters/Axios.adapter';
 
 export const getGamesList = async () => {
   try {
-      const resp = await AxiosAdapter.get('/');
-      console.log('resp', resp);
-      // return [null, resp.data];
+      const resp = await AxiosAdapter.get('/games');
+
+      return [null, resp.data];
   } catch (err) {
       console.log('err', err);
+      return [
+        {
+            type: 'FAILED_ITEMS_FETCH',
+            message: err.response?.data.error,
+        },
+        null,
+    ];
   }
 };
