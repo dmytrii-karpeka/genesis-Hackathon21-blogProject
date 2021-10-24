706192933a1b3f64ae3d74a6c077645746bbeb91
deysan <ideysan@gmail.com>
2021-10-24 17:46:18 +0300

    Clean code

--- my-app/components/Header/Header.jsx
+++ my-app/components/Header/Header.jsx
@@ -9,29 +9,23 @@ const Header = () => {
             <Container>
                 <Navbar.Brand href="/">
                     <img src="https://www.deltafaucet.com/themes/custom/delta_theme/logo.svg"
-                        width="140"
-                        height="35"
-                        className="d-inline-block align-top"
-                        alt="Delta app logo"/>
+                         width="140"
+                         height="35"
+                         className="d-inline-block align-top"
+                         alt="Delta app logo"/>
                 </Navbar.Brand>
                 <Nav>
                     <Link href="/profile">
                         <div className={css.profileLink}>
-                            <Image src="https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-Resize-7IYhhOMpQTI83u.jpg"
-                                    width="40"
-                                    height="40"
-                                    className="d-inline-block align-top"
-                                    alt="Profile" roundedCircle/>
+                            <Image
+                                src="https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-Resize-7IYhhOMpQTI83u.jpg"
+                                width="40"
+                                height="40"
+                                className="d-inline-block align-top"
+                                alt="Profile" roundedCircle/>
                         </div>
                     </Link>
                 </Nav>
-                {/*<Nav>*/}
-                    {/*<Nav.Item>*/}
-                    {/*    <Nav.Link href="/login">*/}
-                    {/*        <Button variant="primary">Sign In</Button>*/}
-                    {/*    </Nav.Link>*/}
-                    {/*</Nav.Item>*/}
-                {/*</Nav>*/}
             </Container>
         </Navbar>
     )
--- my-app/components/ViewItem/ViewItem.jsx
+++ my-app/components/ViewItem/ViewItem.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import ShowMoreText from 'react-show-more-text';
 import {Button, Col, Container, FormControl, Image, InputGroup, Row} from "react-bootstrap";
 
 import css from './ViewItem.module.scss';
 
-const ViewItem = function ({ info }) {
+const ViewItem = function ({info}) {
     const [language, setLanguage] = useState('en');
 
     useEffect(() => {
@@ -13,29 +13,33 @@ const ViewItem = function ({ info }) {
         }
     }, []);
 
-    return(
+    return (
         <Container className={css.container}>
             <Row>
                 <Col sm={1}>
 
                 </Col>
                 <Col sm={4}>
-                    <Image src={info.thumbnail} alt={"Something went wrong?"} rounded fluid />
+                    <Image src={info.thumbnail} alt={"Something went wrong?"} rounded fluid/>
                 </Col>
                 <Col sm={6}>
                     <Row>
                         <h1>{info.title}</h1>
                     </Row>
                     <Row>
-                        <p>Release date: {new Date(info.release_date).toLocaleDateString(language, { day: '2-digit', month: 'short', year: 'numeric' })}</p>
+                        <p>Release date: {new Date(info.release_date).toLocaleDateString(language, {
+                            day: '2-digit',
+                            month: 'short',
+                            year: 'numeric'
+                        })}</p>
                     </Row>
                     <Row>
-                    <ShowMoreText
-                        more={<span className={css.collapseLink}>Show more</span>}
-                        less={<span className={css.collapseLink}>Show less</span>}
-                        className={css.content}>
-                        {info.description}
-                    </ShowMoreText>
+                        <ShowMoreText
+                            more={<span className={css.collapseLink}>Show more</span>}
+                            less={<span className={css.collapseLink}>Show less</span>}
+                            className={css.content}>
+                            {info.description}
+                        </ShowMoreText>
                     </Row>
                     <Row>
                         <Button size="lg" href={info.game_url} target="_blank">
