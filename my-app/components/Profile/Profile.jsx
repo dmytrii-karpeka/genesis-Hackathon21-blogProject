6665f195bc1d7b2105f785c9ef962b2f8b0bd183
Sergey Skvortsov <skvortsov.serge@gmail.com>
2021-10-24 14:40:49 +0300

    connect dropdowns and profile page

--- my-app/components/Profile/Profile.jsx
+++ my-app/components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import {Card, Accordion, Image} from "react-bootstrap";
+import {Card, Image} from "react-bootstrap";
 
 import css from './Profile.module.scss';
 
@@ -6,7 +6,6 @@ const ProfileInfo = function () {
 
     return (
         <Card className="text-center">
-            <Card.Header>Profile Information</Card.Header>
             <Card.Body>
                 <Image src="https://via.placeholder.com/150" roundedCircle/>
                 <Card.Title>Username</Card.Title>
